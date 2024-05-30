4596ebd560a8bcb70ccec6f9982fa5bb2e40eea5
Patrick Lungu <patricklungu99@gmail.com>
2024-05-30 17:27:52 +0200

    All functionalities add

--- Frontend/index.js
+++ Frontend/index.js
@@ -2,13 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const cityInput = document.getElementById('city-input');
     const cityBtn = document.getElementById('city-btn');
     const weatherDetails = document.getElementById('weather-details');
-    const favoriteCities = document.getElementById('favorite-cities');
+    const favoriteSection = document.getElementById('favorite-cities');
     const quotesContainer = document.getElementById('quotes');
 
     fetchMotivationalQuote();
     getFavouriteCities();
     setInterval(fetchMotivationalQuote, 50000);
-
 
     cityBtn.addEventListener('click', (event) => {
         event.preventDefault();
@@ -18,7 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-
+    
     async function getWeather(city) {
         const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=' + city;
         const options = {
@@ -28,24 +27,24 @@ document.addEventListener('DOMContentLoaded', () => {
                 'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
             }
         };
-
+        
         try {
             const response = await fetch(url, options);
             
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
             }
-
+            
             const result = await response.json();
             const current = result.current;
             const location = result.location;
             const currentCondition = result.current.condition;
-
+            
             weatherDetails.innerHTML = `
-                <article>
+            <article>
                     <header>
                         <div>
-                            <img src='${currentCondition.icon}' /></br>
+                        <img src='${currentCondition.icon}' /></br>
                             <p>${location.name}, ${location.country} </p>
                         </div>
                         <div id='second_div'>
@@ -53,25 +52,25 @@ document.addEventListener('DOMContentLoaded', () => {
                             Celcius: ${current.temp_c}°C </br>
                             Fahrenheit: ${current.temp_f}f  </br
                             ${currentCondition.text}>
-                        </div>
-                    </header>
-                    <hr/>
-                    <footer class"region">
-                        Region: ${location.region} </br>
-                        Time: ${location.localtime}
-                    </footer>
-                </article>
-            `
-            const thumbButton = document.getElementById('thumbButton');
-            thumbButton.addEventListener('click', () => addToFavourite(location.name));
-
-        } catch (error) {
+                            </div>
+                            </header>
+                            <hr/>
+                            <footer class"region">
+                            Region: ${location.region} </br>
+                            Time: ${location.localtime}
+                            </footer>
+                            </article>
+                            `
+                            const thumbButton = document.getElementById('thumbButton');
+                            thumbButton.addEventListener('click', () => addToFavourite(location.name));
+                            
+                        } catch (error) {
             console.error(error);
         }
 
         
     }
-
+    
     async function fetchMotivationalQuote() {
         const url = 'https://get-quotes-api.p.rapidapi.com/random';
         const options = {
@@ -81,7 +80,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 'X-RapidAPI-Host': 'get-quotes-api.p.rapidapi.com'
             }
         };
-
+        
         try {
             const response = await fetch(url, options);
             
@@ -99,7 +98,7 @@ document.addEventListener('DOMContentLoaded', () => {
             console.error(error);
         }
     }
-
+    
     async function addToFavourite(city) {
         const url = 'http://localhost:8000/api/favorites';
         const data = { city };
@@ -110,22 +109,22 @@ document.addEventListener('DOMContentLoaded', () => {
             },
             body: JSON.stringify(data)
         };
-    
+        
         try {
             const response = await fetch(url, options);
-    
+            
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
             }
-    
+            
             const result = await response.json();
-            console.log(result);
+            displayFavourite(result.favorites)
         } catch (error) {
             console.error('Error adding to favorites:', error);
         }
     }
     
-
+    
     async function getFavouriteCities() {
         const url = 'http://localhost:8000/api/favorites';
         const options = {
@@ -135,19 +134,67 @@ document.addEventListener('DOMContentLoaded', () => {
                 "Content-Type": "application/json",
             }
         };
-    
+        
         try {
             const response = await fetch(url, options);
-
+            
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
             }
 
             const result = await response.json();
-            console.log(result);
+            displayFavourite(result.favorites)
         } catch (error) {
             console.error('Error fetching favorite cities:', error);
         }
     }
+    
+    function displayFavourite(favoriteCities){
+        if(favoriteCities === undefined || favoriteCities.length === 0){
+            favoriteSection.innerHTML = `<p>No favourite city</p>`;
+            return;
+        }
+        
+        favoriteCities.map((item, index)=>{
+            const node = document.createElement("li");
 
+            const cityName = document.createElement("span");
+            cityName.textContent = item.city;
+
+            const deleteButton = document.createElement("button");
+            deleteButton.textContent = "Delete";
+            deleteButton.className = "delete-button"
+            deleteButton.onclick = () => {
+                deleteFavourite(item.id)
+            };
+
+            node.appendChild(cityName);
+            node.appendChild(deleteButton);
+            favoriteSection.appendChild(node);
+        })
+    }
+
+    async function deleteFavourite(id){
+        const url = `http://localhost:8000/api/favorites/${id}`;
+        const options = {
+            mode: 'cors',
+            method: 'DELETE',
+            headers: {
+                "Content-Type": "application/json",
+            }
+        };
+        
+        try {
+            const response = await fetch(url, options);
+            
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const result = await response.json();
+            displayFavourite(result.favorites)
+        } catch (error) {
+            console.error('Error fetching favorite cities:', error);
+        }
+    }
 });
